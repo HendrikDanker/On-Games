70f3e5979a2b5c13b0cb0f9cef2a2e2368428d0c
simon <simonbauer83@gmail.com>
2017-10-18 12:57:49 +0200

    View/Camera added

--- sourcefiles/GXPEngine/Level.cs
+++ sourcefiles/GXPEngine/Level.cs
@@ -5,11 +5,34 @@ namespace GXPEngine
 {
 	public class Level : GameObject
 	{
+		private Player _player;
+		private GameObject _focus;
 		private LevelCreator _levelCreator;
 		public Level()
 		{
 			_levelCreator = new LevelCreator();
 			AddChild( _levelCreator );
+
+			AddChild( _player );
+			_focus = _player;
+
+			game.OnAfterStep += UpdateCamera;
+		}
+
+		void Update()
+		{
+
+		}
+
+		private void UpdateCamera()
+		{
+			if (_focus != null)
+			{
+				x = (game.width / 2 - _focus.x);
+				y = (game.height / 2 - _focus.y);
+
+			}
+
 		}
 	}
 }
